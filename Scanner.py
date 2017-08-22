fb98d2f21bde07e19058df0fea19b9ddbf8d62c2
anthonylamme <anthonylamme@gmail.com>
2017-08-22 15:30:07 -0500

    Update Scanner.py

--- Scanner.py
+++ Scanner.py
@@ -11,7 +11,7 @@ webAddress='http://gmail.com' #website
 filepathway='/home/pi/SlackData/Data/'
 BaudRate=115200
 
-driver=webdriver.Chromium() #browser
+driver=webdriver.Chrome() #browser
 driver.implicitly_wait(10) #wait for browser to open
 driver.get(webAddress) #go to website
 print driver.content
